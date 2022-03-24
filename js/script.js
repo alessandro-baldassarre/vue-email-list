99bc7308e67c8f134de2233445902fb6d585d500
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-03-24 14:43:11 +0100

    random list generation through boolean API + bonus

--- js/script.js
+++ js/script.js
@@ -1,21 +1,40 @@
 
 
+// Descrizione:
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+// Generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
 const app = new Vue({
     el: '#app',
 
     
     data: {
 
+        emailList: [],
         
     },
 
     methods: {
 
-        
+        createRandomList: function(){
+
+            const self = this;
+
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(function (randomEmail) {
+                self.emailList.push(randomEmail.data.response);
+                })
+            }
+        }
     },
 
-    beforeMount(){
-        
+    mounted(){
+        this.createRandomList();
      },
 
   });
